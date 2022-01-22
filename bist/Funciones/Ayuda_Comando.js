869edd1a38b587aba95263e4be718aaabff1dd79
chiho-san <94724487+chiho-san@users.noreply.github.com>
2022-01-22 17:21:43 -0500

    Update Ayuda_Comando.js

--- bist/Funciones/Ayuda_Comando.js
+++ bist/Funciones/Ayuda_Comando.js
@@ -11,8 +11,36 @@ module.exports = {
         /// Aqui viene lo pro
 
         .addFields(
-            {name: config.Comandos.aBan, value: "Esta tarea tiene dos parametros, el primero es la mencion a la persona quien recibira el Ban y el segundo es una razón por la cual sera baneado"},
-            {name: config.Comandos.meme, value: "Tiene un total de " + config.imagenes.memes.length + " memes guardados en el comando. Disfruta de esos " + config.imagenes.memes.length + "!"}
+            // Comando ">> Ban"
+            {name:
+                config.Comandos.aBan,
+            value: "Esta tarea tiene dos parametros, el primero es la mencion a la persona quien recibira el Ban y el segundo es una razón por la cual sera baneado. Este comando solo esta disponible para los roles de **Owner**, **Moderador** y **Banw**."
+            },
+            // Comando ">> meme"
+            {
+                name: config.Comandos.meme,
+                value: "Tiene un total de " + config.imagenes.memes.length + " memes guardados en el comando. Disfruta de esos " + config.imagenes.memes.length + " memes!"
+            },
+            // Acerca del "Rastreador de mensajes eliminados"
+            {
+                name: 'Detección de mensajes borrados',
+                value: 'Este no es un comando, es un **Tarea** automatica que se dispara cuando un mensaje se elimina ( Solo funciona con los mensajes que son mandados despues de que este bot se haya unido al Servidor ), ejecuta la **Tarea** de mandar un mensaje al canal donde se a eliminado el mensaje y el mensaje lleva el Nombre del usuario que elimino el mensaje y tambien el contenido del mensaje. Esta **Tarea** solo funciona para los mensajes recientes despues de que este bot se haya unido al servidor.'
+            },
+            // Comando momos
+            {
+                name: '>> momos',
+                value: 'Muestra el numero de memes que hay guardados en el comando **">> memes"**.'
+            },
+            // Comando de revision del "Rastreador de mensajes eliminados"
+            {
+                name: '^k',
+                value: 'Este comando mostrara si el **"Rastreador de mensajes eliminados"** esta activada o desactivada. Si esta activada mandara un mensaje diciendo que esta opcion ya esta activada globalmente de lo contrario si no esta activada, mandara un mensaje dando instrucciones de como activar el "Rastreador de mensajes" y tambien mencionara que la opción esta desactivada.'
+            },
+            // Comando de reversion del "Rastreador de mensajes"
+            {
+                name: 'k^T',
+                value: 'Si el **"Rastreador de mensajes eliminados"** esta desactivado, puedes activarla con este comando.'
+            }
         )
 
         /// Se acaba lo pro
@@ -21,4 +49,3 @@ module.exports = {
 
         msg.channel.send({ embeds: [Embed] });
     }
-}
